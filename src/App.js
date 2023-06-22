51dcda9ea157031a6e6b62115755dd345ce1559c
NikolaLoncarski <nidzafrom023@gmail.com>
2023-06-22 21:18:31 +0200

    reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import LoginPage from "./pages/formFields";
+import LoginPage from "./pages/login/LogIn";
 import SignUp from "./pages/login/SignUp";
+
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<SignUp />}>
+      {/* <Route index element={<Home />} /> */}
+      <Route path="login" element={<LoginPage />} />
+      {/* <Route path="crew" element={<Crew />} />
+      <Route path="technology" element={<Technology />} /> */}
+    </Route>
+  )
+);
 function App() {
   return (
     <div>
-      <SignUp />
+      <RouterProvider router={router} />
     </div>
   );
 }
